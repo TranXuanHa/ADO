121f72af932e604dbf74cf131effafbd77d7bb49
TranXuanHa <63442890+TranXuanHa@users.noreply.github.com>
2021-06-05 09:53:23 +0700

    Add files via upload

---
+++ DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QLLichCongTac
+{
+    public class DBConnect
+    {
+        public SqlConnection _conn = new SqlConnection("Data Source=DESKTOP-3AABFT6;Initial Catalog=LCT;Integrated Security=True");
+    }
+}
